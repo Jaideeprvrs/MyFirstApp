10e8f2fe5c133fc7f048c72243428f50f73a94ec
Jaideeprvrs <rvrsjaideep@gmail.com>
2025-11-22 08:40:31 +0530

    weather initial stage

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,24 +1,46 @@
-import { StyleSheet } from "react-native";
-
-import ParallaxScrollView from "@/components/parallax-scroll-view";
-import { ThemedText } from "@/components/themed-text";
-import { IconSymbol } from "@/components/ui/icon-symbol";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 
 export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
+  const [weather, setWeather] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      // 1. Ask permission
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission denied: Allow location to get weather");
+        return;
       }
-    >
-      <ThemedText>Testing OTA Updated with Expo</ThemedText>
-    </ParallaxScrollView>
+
+      // 2. Get user location
+      const loc = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = loc.coords;
+
+      console.log("User Location:", latitude, longitude);
+
+      // 3. Build Open-Meteo API URL dynamically
+      const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+
+      // 4. Fetch weather
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setWeather(data.current_weather);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (!weather) return <ActivityIndicator size="large" />;
+  return (
+    <View style={{ marginTop: 100, alignItems: "center" }}>
+      <Text style={{ fontSize: 24 }}>🌤 Weather</Text>
+      <Text style={{ fontSize: 18 }}>Temp: {weather?.temperature}°C</Text>
+      <Text style={{ fontSize: 18 }}>Wind: {weather?.windspeed} km/h</Text>
+    </View>
   );
 }
 
